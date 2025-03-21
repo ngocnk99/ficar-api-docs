d69686e0bfb9e1d14096f310027dad6268a67f01
ngocnk99 <ngocnk99@gmail.com>
2025-03-21 18:03:38 +0700

    update merchant/promotion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ExampleModule } from './example/example.module';
+import { PromotionModule } from './merchant/promotion/promotion.module';
 
 @Module({
-  imports: [ExampleModule],
+  imports: [ExampleModule, PromotionModule],
 })
 export class AppModule {}
